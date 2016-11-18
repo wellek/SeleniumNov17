1347a67a230c81b386ab934d82c7ce8ff18812ee
Irina <wellek@live.com>
2016-11-18 16:56:51 -0500

    added message to UploadFile

--- TestNGBasic/src/demo/FileUpload.java
+++ TestNGBasic/src/demo/FileUpload.java
@@ -1,5 +1,22 @@
 package demo;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+
 public class FileUpload {
 
+	// new info
+	WebDriver driver; // declare webdriver at class level
+	// otherwise error: driver cannot be resolved
+
+	@BeforeTest // precondition
+	public void openBrowser(){ 
+	driver = new FirefoxDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.get("http://www.barnesandnoble.com/");
+}
 }
